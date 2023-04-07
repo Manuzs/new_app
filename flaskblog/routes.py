5b9a28e93157c59864ccd0d0ac1223bc2ca882c6
Manuzs <ranjitdav350@gmail.com>
2023-04-07 22:40:39 +0530

    Create, Update, Delete Post Done

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -13,10 +13,6 @@ import random
 @app.route("/home")
 def home():
     posts = Post.query.all()
-    print(posts)
-    print(type(posts))
-    return [type(posts)]
-    return posts
     return render_template('home.html', posts = posts)
 
 
@@ -109,8 +105,45 @@ def new_post():
         db.session.commit()
         flash('Post created', 'success')
         return redirect(url_for('home'))
-    return render_template('create_post.html', title = "New Post", form = form)
+    return render_template('create_post.html', title = "New Post", form = form, legend = 'New Post')
 
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title = post.title, post = post)
+
+
+@app.route("/post/<int:post_id>/update", methods = ['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Post Updated', 'success')
+        return redirect(url_for('post', post_id = post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title = 'Update Post', form = form, legend = 'Update Post')
+    
+
+
+@app.route("/post/<int:post_id>/delete", methods = ['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted', 'success')
+    return redirect(url_for('home'))
 
 
 @app.route("/random", methods = ['GET', 'POST'])
@@ -131,7 +164,6 @@ def random1():
                 <body>
                     
                 <h2>A basic HTML table</h2>
-
                 <table style="width:100%">
                 
     
@@ -154,4 +186,4 @@ def random1():
 
     return mstring+title+mstring2+mstring3+mstring4+mstring5
        
-    return render_template('create_post.html', title = "New Post", form = form)
+    return render_template('create_post.html', title = "New Post", form = form)    
